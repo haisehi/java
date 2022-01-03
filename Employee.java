3211ceb074d7843712d23b42dcb01be487b437b0
haisehi <88788629+haisehi@users.noreply.github.com>
2022-01-03 21:03:40 +0700

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,37 +1,60 @@
-package Test.OOP.Inheritance;
+package AWTBasic;
 
-import java.util.Scanner;
+import java.awt.Button;
+import java.awt.Container;
+import java.awt.GridLayout;
 
-public class Employee extends Person {
-	private double works ;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
-	public double getWorks() {
-		return works;
-	}
-
-	public void setWorks(double works) {
-		this.works = works;
-	}
-	//default contructor
+public class Employee extends JFrame {
+	JFrame f = new JFrame("Employee Details ");
+	JLabel lbUser = new JLabel("Name");
+	JTextField tfUser = new JTextField(10);
+	JLabel lbGender = new JLabel("Gender");
+	JRadioButton rdoMale = new JRadioButton("Male");
+	JRadioButton rdoFemale = new JRadioButton("Female");
+	ButtonGroup bgGender = new ButtonGroup();
+	String grade[] = {"Elementary","High school","Undergraduate","Mater","Doctor"};
+	JLabel lblGrade = new JLabel("Grade");
+	JList lstGrade = new JList(grade);
+	
+	JLabel lblDepartment = new JLabel("Department");
+	String department[] = {"VKU","Axon","FPT","Enclave"};
+	JComboBox cboDepartment = new JComboBox(department);
+	
+	JButton btnSave = new JButton("Add employee");
+	
+	
 	public Employee() {
-		this.Input();
-		this.Output();
+		Container cont = f.getContentPane();
+		f.setLayout(new GridLayout(6,2));
+		f.setLocation(300,100);
+		f.add(lbUser);
+		f.add(tfUser);
+		f.add(lbGender);
+		f.add(rdoMale);
+		f.add(rdoFemale);
+		bgGender.add(rdoMale);
+		bgGender.add(rdoFemale);
+		f.add(lblGrade);
+		f.add(lstGrade);
+		f.add(lblDepartment);
+		f.add(cboDepartment);
+		f.add(btnSave);
+		f.setSize(300,700);
+		f.pack();
+		f.setVisible(true);
 	}
-	public Employee(String name ,double rate , double works) {
-		super(name , rate);
-		this.works = works;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Employee();
 	}
-	public void Input() {
-		super.Input();
-		Scanner key = new Scanner(System.in);
-		System.out.println("works = ");
-		this.works = Double.parseDouble(key.nextLine());
-	}
-	public double CalSalary() {
-		return super.rate*1500*this.works;
-	}
-	public void Output() {
-		super.Output();
-		System.out.println(" has works ="+this.getWorks()+" has Salary="+this.CalSalary());
-	}
+
 }
